a6dba8ba59e5accb5679fe479400ec443c93f917
Henry Charles <HenryTech@outlook.com>
2015-03-22 09:46:36 +0800

    created MongoDbDataWarehouse class #5

---
+++ src/Nova/Data/MongoDbDataWarehouse.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nova.Data
+{
+    public class MongoDbDataWarehouse :IDataWarehouse
+    {
+        public Task SaveAsync<T>(T entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeleteAsync<T>(T entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IQueryable<T>> QueryAsync<T>()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
